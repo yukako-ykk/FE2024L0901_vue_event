47d91981d017590b80edc0c5d1e791e4c0b6ecf0
yukako-ykk <2301188@s.asojuku.ac.jp>
2024-10-10 12:41:44 +0900

    3-1と3-2を作成

--- 3-1/script/script.js
+++ 3-1/script/script.js
@@ -3,7 +3,12 @@ new Vue({
     el: '#app',
     data() {
         return {
-            
+            text: 'hello Vue.js'
         };
+    },
+    methods: {
+        handleClick(){
+            this.text='button pushed';
+        }
     }
 });
--- 3-2/script/script.js
+++ 3-2/script/script.js
@@ -1,1 +1,17 @@
 // JavaScript用ファイル
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            count: 0
+        };
+    },
+    methods:{
+        increment(){
+            this.count++;
+        },
+        decrement(){
+            this.count--;
+        }
+    }
+});
